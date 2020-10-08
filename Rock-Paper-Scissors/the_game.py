733793ea90e2e92f914d44337ea8a511c26bcb4a
pavel.vyukov <vukov87@mail.ru>
2020-10-08 09:29:13 +0300

    Second task: real game.

--- Rock-Paper-Scissors/the_game.py
+++ Rock-Paper-Scissors/the_game.py
@@ -1,3 +1,12 @@
-choose = input()
+import random
+
+user = input()
+options = ('rock', 'paper', 'scissors')
+comp = random.choice(options)
 winners = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
-print(f'Sorry, but the computer chose {winners[choose]}')
+if user == comp:
+    print(f'There is a draw ({comp})')
+elif winners[user] == comp:
+    print(f'Sorry, but the computer chose {comp}')
+else:
+    print(f'Well done. The computer chose {comp} and failed')
